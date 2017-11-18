cdbd19d3fd3c87ac78ffaa539d5fcc4c2f6502d9
mazhaohai <13466763005@163.com>
2017-11-18 18:20:32 +0800

    update deps

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,31 +1,17 @@
 <template>
 	<div>
 		<h1>{{msg}}</h1>
-		<div class="marquee-wrap" style="width: 100px;">
-			<vue-marquee content="33333" class="two" :showtwo="false"></vue-marquee>
-		</div>
-		<div class="marquee-wrap" style="width: 100px;">
-			<vue-marquee content="22222" class="two" :showtwo="false"></vue-marquee>
-		</div>
-		<div class="marquee-wrap" style="width: 100px;">
-			<vue-marquee content="1" class="two" :showtwo="false"></vue-marquee>
-		</div>
 	</div>
 
 </template>
 
 <script>
-	import VueMarquee from 'vue-marquee-ho';
-	import Css from 'vue-marquee-ho/dist/vue-marquee.min.css'
 	export default {
 		name: 'Hello',
 		data() {
 			return {
 				msg: 'Hello world！！！'
 			}
-		},
-		components: {
-			VueMarquee
 		}
 	}
 </script>
